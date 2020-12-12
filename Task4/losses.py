ca9f7e8b7331ca3df6caa6d28b6fe5d9422951d3
winter <q916090796@gmail.com>
2020-12-12 21:45:24 +0800

    change CrossEntropLoss to NLLLoss

--- Task4/losses.py
+++ Task4/losses.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 class MaskLoss(nn.modules.loss._Loss):
     def __init__(self):
         super().__init__()
-        CEL=nn.CrossEntropyLoss(reduce=False,reduction='none')
+        CEL=nn.NLLLoss(reduce=False,reduction='none')
 
 
     def forward(output,target,mask):
