81358b12eb5e17923edfb0d44eab7f65b97b89b8
winter <q916090796@gmail.com>
2020-12-24 20:43:57 +0800

    fix utils

--- Task3/utils.py
+++ Task3/utils.py
@@ -6,10 +6,14 @@ import config
 def load_model(model,load_dir,name):
     if not os.path.exists(load_dir):
         os.makedirs(load_dir)
-    model.load_state_dict(torch.load(os.path.join(load_dir,name),map_location=config.DEVICE))
+
+    model.load_state_dict(torch.load(os.path.join(load_dir,name),
+                                     map_location=config.DEVICE))
+
 
 def save_model(model,save_dir,name):
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
+
     torch.save(model.state_dict(),os.path.join(save_dir,name))
     
--- Task4/utils.py
+++ Task4/utils.py
@@ -8,7 +8,8 @@ def load_model(model,load_dir,name):
     if not os.path.exists(load_dir):
         os.makedirs(load_dir)
 
-    model.load_state_dict(torch.load(os.path.join(load_dir,name)),map_location=config.DEVICE)
+    model.load_state_dict(torch.load(os.path.join(load_dir,name),
+                                     map_location=config.DEVICE))
 
 
 def save_model(model,save_dir,name):
